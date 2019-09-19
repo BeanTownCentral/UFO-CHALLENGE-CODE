cc6a08cae8e5e41a33f7036619fa473fcf6e8f20
Joshua.Bonilla_UCF <54584900+BeanTownCentral@users.noreply.github.com>
2019-09-19 17:57:53 -0400

    Ufo_CHallenge_Code
    Code_UFOCHallenge

---
+++ UFO_CHALLENGE/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public Text countText;
+	public Text livesText;
+	public Text winText;
+
+	private Rigidbody2D rb2d;
+	private int count;
+	private int lives;
+	
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		lives = 3;
+		winText.text = "";
+		SetCountText ();
+		SetLivesText ();
+	}
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal,moveVertical);
+		rb2d.AddForce (movement * speed);
+		{
+			if (Input.GetKey("escape"))
+			{
+		
+				Application.Quit();
+			}
+		}
+	}
+
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+		 if (other.gameObject.CompareTag("Enemy"))
+		{
+          other.gameObject.SetActive(false);
+          lives = lives - 1;
+		  SetLivesText();
+		}
+		if (count == 12)
+		{
+			transform.position = new Vector2(1.0f, -57.08f);
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 20)
+		{
+			winText.text = "You win! Game created by Joshua Bonilla!";
+		}
+	}
+	void SetLivesText()
+	{
+		livesText.text = "Lives: " + lives.ToString ();
+		if (lives == 0)
+		{
+			Object.Destroy(gameObject);
+			winText.text = "You Lose!";
+		}
+	}
+}
